6e2dad4d9e11f79c6706ffaf938885318d24448f
Sjoerd Wiecherink <sjoerdwiecherink@gmail.com>
2024-04-03 16:45:02 +0200

    basic Flask

---
+++ Flask_app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+if __name__ == '__main__':    
+    app.run(port=5000,debug=True)
+
+@app.route('/')
+def home():    
+    return render_template("<p>Hello world!</p>")
+
+
+
